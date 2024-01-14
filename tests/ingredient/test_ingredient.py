2d6d77ffe108790ad18530f6a5f6a9af7dd7459f
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-14 11:28:23 -0300

    Feat: Requisito 01 finalizado

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+from src.models.ingredient import restriction_map
 
 
-# Req 1
 def test_ingredient():
-    pass
+    flour = Ingredient('farinha')
+    bacon = Ingredient('bacon')
+    cheese = Ingredient('queijo mussarela')
+
+    assert flour == flour
+    assert cheese != bacon
+
+    assert bacon.name == 'bacon'
+    assert hash(bacon) == hash('bacon')
+    assert repr(bacon) == "Ingredient('bacon')"
+
+    assert flour.name == 'farinha'
+    assert hash(flour) == hash('farinha')
+    assert repr(flour) == "Ingredient('farinha')"
+
+    assert cheese.restrictions == restriction_map().get('queijo mussarela')
+    assert flour.restrictions == restriction_map().get('farinha')
+    assert bacon.restrictions == restriction_map().get('bacon')
