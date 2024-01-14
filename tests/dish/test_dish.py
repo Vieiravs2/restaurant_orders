d2516e94baff5a89183a8957cf0843dbc9a86e87
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-14 12:27:16 -0300

    Fix(Refactor): Requisito 02

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -5,28 +5,24 @@ import pytest
 
 def test_dish():
     picanha = Dish("Picanha a Moda", 100.0)
-    maminha = Dish("Maminha a Moda", 80.0)
     cheese = Ingredient("queijo mussarela")
     invalidPrice = 'Dish price must be float.'
     lowerOrEqualZero = 'Dish price must be greater then zero.'
 
     with pytest.raises(TypeError, match=invalidPrice):
-        Dish("Invalid Dish", "ThisIsString")
+        Dish("Invalid Dish", "Price being a text")
 
     with pytest.raises(ValueError, match=lowerOrEqualZero):
-        Dish("Prato sem valor", 0)
+        Dish("Worthless dish", 0)
 
     assert picanha.name == "Picanha a Moda"
     assert picanha.price == 100.0
 
-    assert maminha.name == "Maminha a Moda"
-    assert maminha.price == 80.0
-
-    assert picanha != maminha
+    assert picanha != Dish("Maminha a Moda", 80.0)
     assert picanha == Dish("Picanha a Moda", 100.0)
 
     assert hash(picanha) == hash(Dish("Picanha a Moda", 100.0))
-    assert hash(picanha) != hash(maminha)
+    assert hash(picanha) != hash(Dish("Maminha a Moda", 80.0))
 
     assert repr(picanha) == "Dish('Picanha a Moda', R$100.00)"
 
