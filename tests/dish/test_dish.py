121a7ac4d556c094cfcd3b58911d2b2579691913
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-14 12:23:10 -0300

    Feat: Requisito 02 finalizado

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,38 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    picanha = Dish("Picanha a Moda", 100.0)
+    maminha = Dish("Maminha a Moda", 80.0)
+    cheese = Ingredient("queijo mussarela")
+    invalidPrice = 'Dish price must be float.'
+    lowerOrEqualZero = 'Dish price must be greater then zero.'
+
+    with pytest.raises(TypeError, match=invalidPrice):
+        Dish("Invalid Dish", "ThisIsString")
+
+    with pytest.raises(ValueError, match=lowerOrEqualZero):
+        Dish("Prato sem valor", 0)
+
+    assert picanha.name == "Picanha a Moda"
+    assert picanha.price == 100.0
+
+    assert maminha.name == "Maminha a Moda"
+    assert maminha.price == 80.0
+
+    assert picanha != maminha
+    assert picanha == Dish("Picanha a Moda", 100.0)
+
+    assert hash(picanha) == hash(Dish("Picanha a Moda", 100.0))
+    assert hash(picanha) != hash(maminha)
+
+    assert repr(picanha) == "Dish('Picanha a Moda', R$100.00)"
+
+    picanha.add_ingredient_dependency(cheese, 2)
+
+    assert len(picanha.recipe) == 1
+    assert len(picanha.get_ingredients()) == 1
+    assert len(picanha.get_restrictions()) == 2
+
